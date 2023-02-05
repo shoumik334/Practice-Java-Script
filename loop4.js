fc5f480fe4dfb93211e7ec0b56cabe31fd6503ef
Sadaen Parves Shoumik <sadaen.shoumik@northsouth.edu>
2023-02-05 14:20:30 +0600

    Update loop4.js

--- loop4.js
+++ loop4.js
@@ -1,8 +1,11 @@
 var num=[1,2,3,4,5,6,7,7,8];
-num.pop();
+var popout=num.pop(1);
+var t='pop out element'
 
 
-for(i=0;i<9;i++)
+for(i=0;i<8;i++)
 {
     console.log(num[i]);
 }
+console.log(t)
+console.log(popout);
